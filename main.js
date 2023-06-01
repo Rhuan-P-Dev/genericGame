745c77ed2018735c6e4cb5abaceab7a8a96f7b08
Rhuan P <noreplay@example.com>
2023-06-01 20:58:55 -0300

    inf missi

--- main.js
+++ main.js
@@ -18,6 +18,7 @@ const verde = {
     "life":-1,
     "exe":() => {},
     "ser":() => {},
+    "ar": []
 }
 
 var fMultP = 0.5
@@ -38,59 +39,55 @@ const gameObjects = {
 canvas.addEventListener("click", (e)=>{
 
     if(gameObjects.verde.missi > 0){
-        gameObjects.aaa = {
+        gameObjects.verde.ar[gameObjects.verde.ar.length] = {
             "color":"blue",
             "x":verde.x+(verde.width/2)-10,
             "y":verde.y+(verde.height/2)-10,
-            "width":20,
-            "height": 20,
+            "width":5,
+            "height": 5,
             "xAcc":0,
             "yAcc":0,
             "frente":0,
             "rota":0,
-            "life":2500,
-            "exe":() => {
-                verde.missi += 1
-                delete gameObjects.aaa
+            "life":100,
+            "exe":(eaa) => {
+                
             },
             "target":{"x":e.offsetX,"y":e.offsetY},
             "ser":(eu, alvo) => {
 
-                if(gameObjects.aaa.yAcc < -2){
-                    gameObjects.aaa.yAcc = -2
+                if(eu.yAcc < -2){
+                    eu.yAcc = -2
                 }
         
-                if(gameObjects.aaa.yAcc > 2){
-                    gameObjects.aaa.yAcc = 2
+                if(eu.Acc > 2){
+                    eu.yAcc = 2
                 }
         
-                if(gameObjects.aaa.xAcc < -2){
-                    gameObjects.aaa.xAcc = -2
+                if(eu.xAcc < -2){
+                    eu.xAcc = -2
                 }
         
-                if(gameObjects.aaa.xAcc > 2){
-                    gameObjects.aaa.xAcc = 2
+                if(eu.xAcc > 2){
+                    eu.xAcc = 2
                 }
 
                 if(eu.y < alvo.y){
-                    gameObjects.aaa.yAcc += 0.01
+                    eu.yAcc += 0.05
                 }else{
-                    gameObjects.aaa.yAcc -= 0.01
+                    eu.yAcc -= 0.05
                 }
         
                 if(eu.x < alvo.x){
-                    gameObjects.aaa.xAcc += 0.01
+                    eu.xAcc += 0.05
                 }else{
-                    gameObjects.aaa.xAcc -= 0.01
+                    eu.xAcc -= 0.05
                 }
-
 
             },
         }
 
-        gameObjects.aaa.ser({"x":gameObjects.aaa.x,"y":gameObjects.aaa.y} , gameObjects.aaa.target)
-
-        gameObjects.verde.missi -= 1
+        gameObjects.verde.missi += 1
         
     }
 
@@ -98,7 +95,175 @@ canvas.addEventListener("click", (e)=>{
 
 html.addEventListener("keydown", (e)=>{
 
-    console.log(e)
+    if(e["key"] == "ArrowUp"){
+
+        if(verde.yAcc < -1){
+            verde.yAcc = -1
+        }
+
+        if(verde.yAcc > 1){
+            verde.yAcc = 1
+        }
+
+        if(verde.xAcc < -1){
+            verde.xAcc = -1
+        }
+
+        if(verde.xAcc > 1){
+            verde.xAcc = 1
+        }
+
+        verde.yAcc -= (0.5 * verde.frente)
+        verde.xAcc -= (0.5* verde.rota)
+        
+    }
+
+    if(e["key"] == "ArrowRight"){
+
+        verde.frente += fProx
+
+        if(verde.frente < -1){
+            console.log("AGORA É POSITIVO")
+            fProx = fMultP
+            verde.frente += fProx
+        }
+
+        if(verde.frente > 1){
+            console.log("AGORA É NEGATIVO")
+            fProx = fMultN
+            verde.frente += fProx
+        }
+
+        verde.rota += rProx
+
+        if(verde.rota < -1){
+            console.log("AGORA É POSITIVO")
+            rProx = rMultP
+            verde.rota += rProx
+        }
+
+        if(verde.rota > 1){
+            console.log("AGORA É NEGATIVO")
+            rProx = rMultN
+            verde.rota += rProx
+        }
+
+    }
+
+    if(e["key"] == " "){
+        if(gameObjects.verde.tiros > 0){
+            gameObjects.tiro = {
+                "color":"black",
+                "x":verde.x+(verde.width/2)-5,
+                "y":verde.y+(verde.height/2)-5,
+                "width":10,
+                "height": 10,
+                "xAcc":((-verde.rota * 2) - -verde.xAcc)*1.5,
+                "yAcc":((-verde.frente * 2) - -verde.yAcc)*1.5,
+                "frente":0,
+                "rota":0,
+                "life":150,
+                "exe":() => {
+                    verde.tiros += 1
+                    delete gameObjects.tiro
+                },
+                "ser":() => {},
+            }
+
+            gameObjects.verde.tiros -= 1
+        }
+
+    }
+
+
+})
+
+html.addEventListener("keypress", (e)=>{
+
+    if(e["key"] == "ArrowUp"){
+
+        if(verde.yAcc < -1){
+            verde.yAcc = -1
+        }
+
+        if(verde.yAcc > 1){
+            verde.yAcc = 1
+        }
+
+        if(verde.xAcc < -1){
+            verde.xAcc = -1
+        }
+
+        if(verde.xAcc > 1){
+            verde.xAcc = 1
+        }
+
+        verde.yAcc -= (0.5 * verde.frente)
+        verde.xAcc -= (0.5* verde.rota)
+        
+    }
+
+    if(e["key"] == "ArrowRight"){
+
+        verde.frente += fProx
+
+        if(verde.frente < -1){
+            console.log("AGORA É POSITIVO")
+            fProx = fMultP
+            verde.frente += fProx
+        }
+
+        if(verde.frente > 1){
+            console.log("AGORA É NEGATIVO")
+            fProx = fMultN
+            verde.frente += fProx
+        }
+
+        verde.rota += rProx
+
+        if(verde.rota < -1){
+            console.log("AGORA É POSITIVO")
+            rProx = rMultP
+            verde.rota += rProx
+        }
+
+        if(verde.rota > 1){
+            console.log("AGORA É NEGATIVO")
+            rProx = rMultN
+            verde.rota += rProx
+        }
+
+    }
+
+    if(e["key"] == " "){
+        if(gameObjects.verde.tiros > 0){
+            gameObjects.tiro = {
+                "color":"black",
+                "x":verde.x+(verde.width/2)-5,
+                "y":verde.y+(verde.height/2)-5,
+                "width":10,
+                "height": 10,
+                "xAcc":((-verde.rota * 2) - -verde.xAcc)*1.5,
+                "yAcc":((-verde.frente * 2) - -verde.yAcc)*1.5,
+                "frente":0,
+                "rota":0,
+                "life":150,
+                "exe":() => {
+                    verde.tiros += 1
+                    delete gameObjects.tiro
+                },
+                "ser":() => {},
+            }
+
+            gameObjects.verde.tiros -= 1
+        }
+
+    }
+
+
+})
+
+html.addEventListener("keyup", (e)=>{
 
     if(e["key"] == "ArrowUp"){
 
@@ -184,7 +349,6 @@ html.addEventListener("keydown", (e)=>{
 })
 
 
-
 function gameLoopInit(){
     window.requestAnimationFrame(gameRender)
     setInterval(simu,10)
@@ -194,6 +358,22 @@ function simu(){
 
     for (y in gameObjects) {
         let x = gameObjects[y]
+
+        if(y == "verde"){
+            for (let index = 0; index < x.ar.length; index++) {
+                x.ar[index].x += x.ar[index].xAcc
+                x.ar[index].y += x.ar[index].yAcc
+                x.ar[index].life -= 1
+                if(x.ar[index].life < 0){
+                    x.ar[index].exe(x.ar[index])
+                }
+                x.ar[index].ser(
+                    x.ar[index],
+                    x.ar[index].target
+                )
+            }
+        }
+
         if(x.life < 0){
             x.exe()
         }
@@ -201,9 +381,6 @@ function simu(){
         x.y += x.yAcc
         if(x.life != -1){
             x.life -= 1
-        }
-        if(gameObjects.aaa){
-            gameObjects.aaa.ser({"x":gameObjects.aaa.x,"y":gameObjects.aaa.y} , gameObjects.aaa.target)
         }
         
     }
@@ -215,6 +392,14 @@ function gameRender(){
 
     for (y in gameObjects) {
         let x = gameObjects[y]
+
+        if(y == "verde"){
+            for (let index = 0; index < x.ar.length; index++) {
+                ctx.fillStyle = x.ar[index].color
+                ctx.fillRect(x.ar[index].x, x.ar[index].y, x.ar[index].width, x.ar[index].height)
+            }
+        }
+
         ctx.fillStyle = x.color
         ctx.fillRect(x.x, x.y, x.width, x.height)
 
