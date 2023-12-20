80ac62218d7a8c9a3a5741f47e33959b49cb2c30
Rhuan P <147956448+Rhuan-P-Dev@users.noreply.github.com>
2023-12-19 21:02:55 -0300

    add: bigDrone.js

---
+++ js/object/complex/bigDrone.js
@@ -0,0 +1,46 @@
+import { InheritController } from "../../generalUtils/inherit.js"
+import { DroneV2 } from "./droneV2.js"
+
+export class BigDrone {
+
+    constructor(build = false){
+
+        new InheritController().inherit(
+            this,
+            [
+                DroneV2,
+            ],
+            build
+        )
+
+        // ugly - same stats form shieldObject.js
+        this.shield *= 5
+        this.maxShield *= 5
+        this.shieldRegen *= 5
+
+
+
+        this.vel *= 1.1
+        this.maxVel *= 1.2
+
+        //this.priority += 1 == mesma prioridade que uma fatory
+
+        this.life *= 1.2
+        this.maxLife *= 1.2
+
+        this.defense *= 2
+        this.resistance = 0.97
+
+        this.width += 3
+        this.height += 3
+    
+        this.energy *= 1.1
+        this.maxEnergy *= 1.1
+
+        this.rotationVel *= 1.25
+
+        this.damage *= 2
+
+    }
+
+}
