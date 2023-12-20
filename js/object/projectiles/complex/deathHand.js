e28e874bdd8b68d093ab0689db5f52afcff1b16f
Rhuan P <147956448+Rhuan-P-Dev@users.noreply.github.com>
2023-12-19 21:30:51 -0300

    add: deathHand.js

---
+++ js/object/projectiles/complex/deathHand.js
@@ -0,0 +1,70 @@
+
+import { EffectsController } from "../../../effects/effectsController.js"
+import { InheritController } from "../../../generalUtils/inherit.js"
+import { MovableObject } from "../../basic/movableObject.js"
+import { Rotable } from "../../basic/rotable.js"
+import { BasicProjetile } from "../basic/basicProjetile.js"
+
+var Effects = ""
+
+onInit(function(){
+
+    Effects = new EffectsController()
+
+})
+
+export class DeathHand {
+
+    constructor(build = false){
+
+        new InheritController().inherit(
+            this,
+            [
+                BasicProjetile,
+                Rotable,
+                MovableObject,
+            ],
+            build
+            
+        )
+
+        this.life = 200
+        this.maxLife = 200
+
+        this.width = 6
+        this.height = 6
+
+        this.defense = 0
+        this.damage = 0
+
+        this.lifeTime = 500
+
+        this.maxVel /= 2
+        this.vel *= 2
+        this.rotationVel *= 4
+
+    }
+
+    passBuildList = {
+        ["add_miniWorldFunctions"]: (updateThis) => {
+
+            updateThis.onHit.remove("last", 0) // selfDestruction
+
+            Effects.apply(
+                "onHit",
+                "burn",
+                "effect",
+                {
+                    "object": updateThis,
+                    "damage": 1,
+                },
+                {
+                    "frameOut": 60,
+                    "repeat": -1,
+                },
+            )
+
+        }
+    }
+
+}
