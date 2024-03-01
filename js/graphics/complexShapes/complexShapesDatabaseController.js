18a3e4ede5e807c32c2b6ea282cb7ebb7c8d87b1
Rhuan P <147956448+Rhuan-P-Dev@users.noreply.github.com>
2024-03-01 07:40:02 -0300

    fix(ComplexShapesDatabaseC - applyParams()): avoid bugs

--- js/graphics/complexShapes/complexShapesDatabaseController.js
+++ js/graphics/complexShapes/complexShapesDatabaseController.js
@@ -100,7 +100,7 @@ export class ComplexShapesDatabaseController {
     
     applyParams(object, params){
     
-        object.params.canvasScale = params.canvasScale
+        object.params.canvasScale += params.canvasScale
         object.params.rotation = params.rotation
         object.params.offset = {}
         object.params.offset.x = params.offset.x
