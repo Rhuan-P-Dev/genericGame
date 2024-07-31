4eef1fc53c1d97e34621bf3c2b88fec8cacdee28
Rhuan P <147956448+Rhuan-P-Dev@users.noreply.github.com>
2024-07-31 01:40:02 -0300

    fix(EffectsC - apply()): undefined params.object

--- js/effects/effectsController.js
+++ js/effects/effectsController.js
@@ -169,8 +169,6 @@ export class EffectsController {
                 )
             }
 
-            params.object = undefined
-            
             for (
                 let index = 0;
                 index < objects.length
