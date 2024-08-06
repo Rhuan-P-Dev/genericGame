71c89f64ad32d7656c6e8b60b8bf09b1f69efea9
Rhuan P <147956448+Rhuan-P-Dev@users.noreply.github.com>
2024-08-06 03:37:35 -0300

    add: new factorys
    self swarm sniper
    self swarm melee

---
+++ js/shipUnits/factory/info/factory/selfSwarmMelee.js
@@ -0,0 +1,32 @@
+import { FocusedTopDownBehavior } from "../../../../AI/behavior/focusedTopDownBehavior.js"
+import { InheritController } from "../../../../generalUtils/inherit.js"
+import { SelfSwarmDrone } from "../../../../object/complex/special drone/selfSwarmDrone.js"
+import { FactoryExtend } from "../extend/factory.js"
+
+export class SelfSwarmMelee {
+
+    constructor(build = false){
+
+        new InheritController().inherit(
+            this,
+            [
+                FactoryExtend,
+            ],
+            build
+        )
+
+    }
+
+    name = "self swarm melee"
+    cost = 20
+    reload = 10*60
+
+    config = {
+        "objectClass": SelfSwarmDrone,
+        "AI": ["missileV1",],
+        "activates": {},
+        "behavior": new FocusedTopDownBehavior().searchPriority,
+        "statsMult": 0,
+    }
+
+}
---
+++ js/shipUnits/factory/info/factory/selfSwarmSniper.js
@@ -0,0 +1,34 @@
+import { FocusedTopDownBehavior } from "../../../../AI/behavior/focusedTopDownBehavior.js"
+import { InheritController } from "../../../../generalUtils/inherit.js"
+import { SelfSwarmDrone } from "../../../../object/complex/special drone/selfSwarmDrone.js"
+import { FactoryExtend } from "../extend/factory.js"
+
+export class SelfSwarmSniper {
+
+    constructor(build = false){
+
+        new InheritController().inherit(
+            this,
+            [
+                FactoryExtend,
+            ],
+            build
+        )
+
+    }
+
+    name = "self swarm sniper"
+    cost = 50
+    reload = 20*60
+
+    config = {
+        "objectClass": SelfSwarmDrone,
+        "AI": ["movable","useActivates"],
+        "activates": {
+            "weapon": ["self swarm three sniper"],
+        },
+        "behavior": new FocusedTopDownBehavior().searchPriority,
+        "statsMult": 0.3,
+    }
+
+}
