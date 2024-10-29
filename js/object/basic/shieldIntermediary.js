2e0dbb0fc500728b501e96449f3dfa9fa5c5e867
Rhuan P <147956448+Rhuan-P-Dev@users.noreply.github.com>
2024-10-29 12:59:42 -0300

    add: ShieldIntermediary

---
+++ js/object/basic/shieldIntermediary.js
@@ -0,0 +1,80 @@
+import { InheritController } from "../../generalUtils/inherit.js"
+import { Object } from "./object.js"
+import { DamageController } from "../../damage/damageController.js"
+import { CommonImport } from "../common/commonImport.js"
+
+var Damage
+
+onInit(function(){
+
+    Damage = new DamageController()
+
+})
+
+export class ShieldIntermediary {
+
+    constructor(build = false){
+        
+        new InheritController().inherit(
+            this,
+            [
+                CommonImport
+            ],
+            build
+        )
+
+    }
+
+    shield = 50
+    maxShield = 50
+    shieldRegen = 1 / (1 * 60)
+
+    damageOrderList = [
+        "dark energy",
+        "physical",
+        "fire",
+        "parasite blaster",
+        "shock",
+        "self swarm",
+        "agony",
+        "surprise attack",
+    ]
+
+    passBuildList = {
+
+        "add_shieldConsume": (updateThis) => {
+
+            Damage.addDamageOrder(
+                updateThis,
+                "self swarm production",
+                "shield",
+                "before",
+                "lifeRegen",
+            )
+
+            for (
+                let index = 0;
+                index < this.damageOrderList.length;
+                index++
+            ) {
+
+                Damage.addDamageOrder(
+                    updateThis,
+                    this.damageOrderList[index],
+                    "shield",
+                    "before",
+                    "life",
+                )
+
+            }
+
+            Damage.addDefense(updateThis, "shield", "physical", 0.25)
+            Damage.addDefense(updateThis, "shield", "fire", 2)
+            Damage.addDefense(updateThis, "shield", "self swarm", 0.9)
+            Damage.addDefense(updateThis, "shield", "self swarm production", 1000)
+
+        }
+
+    }
+
+}
--- js/object/basic/shieldObject.js
+++ js/object/basic/shieldObject.js
@@ -1,14 +1,6 @@
 import { InheritController } from "../../generalUtils/inherit.js"
 import { Object } from "./object.js"
-import { DamageController } from "../../damage/damageController.js"
-
-var Damage
-
-onInit(function(){
-
-    Damage = new DamageController()
-
-})
+import { ShieldIntermediary } from "./shieldIntermediary.js"
 
 export class ShieldObject {
 
@@ -17,85 +9,13 @@ export class ShieldObject {
         new InheritController().inherit(
             this,
             [
+                ShieldIntermediary,
                 Object
             ],
             build
         )
 
         this.priority += 1
-
-    }
-
-    shield = 50
-    maxShield = 50
-    shieldRegen = 1 / (1 * 60)
-
-    passBuildList = {
-
-        "add_shieldConsume": (updateThis) => {
-
-            Damage.addDamageOrder(
-                updateThis,
-                "dark energy",
-                "shield",
-                "before",
-                "life",
-            )
-
-            Damage.addDamageOrder(
-                updateThis,
-                "physical",
-                "shield",
-                "before",
-                "life",
-            )
-
-            Damage.addDamageOrder(
-                updateThis,
-                "fire",
-                "shield",
-                "before",
-                "life",
-            )
-
-            Damage.addDamageOrder(
-                updateThis,
-                "parasite blaster",
-                "shield",
-                "before",
-                "life",
-            )
-
-            Damage.addDamageOrder(
-                updateThis,
-                "shock",
-                "shield",
-                "before",
-                "life",
-            )
-
-            Damage.addDamageOrder(
-                updateThis,
-                "self swarm",
-                "shield",
-                "before",
-                "life",
-            )
-
-            Damage.addDamageOrder(
-                updateThis,
-                "self swarm production",
-                "shield",
-                "before",
-                "lifeRegen",
-            )
-
-            Damage.addDefense(updateThis, "shield", "physical", 0.25, true)
-            Damage.addDefense(updateThis, "shield", "fire", 2, true)
-            Damage.addDefense(updateThis, "shield", "self swarm", 0.9, true)
-            Damage.addDefense(updateThis, "shield", "self swarm production", 99999999999999, true)
-
-        }
 
     }
 
