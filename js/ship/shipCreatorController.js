61c836e08cb08a0d3a8ed6c9acb997420c7c1d83
Rhuan P <147956448+Rhuan-P-Dev@users.noreply.github.com>
2024-10-29 14:08:32 -0300

    giga refactor: shipCreatorController.js
    team,
    AIObject = {
        AI: [],
        coreName: "default"
    },
    activates = {},
    effects = {},
    isPlayer,
    type = new Ship(true),
    ascensions = [],
    statsUpgrade = [],
    defenseTypes = [],
    effectsApply = []

--- js/ship/shipCreatorController.js
+++ js/ship/shipCreatorController.js
@@ -1,17 +1,23 @@
+import { AscensionScorerController } from "../../jsWaveGame/scorers/ascensionScorerController.js"
 import { AIController } from "../AI/AIController.js"
+import { DamageController } from "../damage/damageController.js"
 import { EffectsController } from "../effects/effectsController.js"
-import { FrameController } from "../frame/frameController.js"
 import { GameStateController } from "../gameState/gameStateController.js"
+import { CloneObjectController } from "../generalUtils/cloneObject.js"
 import { MultiplyStatsController } from "../generalUtils/multiplyStats.js"
 import { KeyBoardController } from "../keyboard/keyBoardController.js"
 import { Ship } from "../object/complex/ship.js"
 import { ObjectActivatesController } from "../objectController/objectActivatesController.js"
-import { SpecialController } from "../shipUnits/special/specialController.js"
 
-var GameState = ""
-var ObjectActivates = ""
-var AIC = ""
-var KeyBoard = ""
+var GameState
+var ObjectActivates
+var AIC
+var KeyBoard
+var Effects
+var AscensionScorer
+var CloneObject
+var MultiplyStats
+var Damage
 
 onInit(function(){
 
@@ -19,6 +25,11 @@ onInit(function(){
     ObjectActivates = new ObjectActivatesController()
     AIC = new AIController()
     KeyBoard = new KeyBoardController()
+    Effects = new EffectsController()
+    AscensionScorer = new AscensionScorerController()
+    CloneObject = new CloneObjectController()
+    MultiplyStats = new MultiplyStatsController()
+    Damage = new DamageController()
 
 })
 
@@ -26,71 +37,201 @@ export class ShipCreatorController{
 
     mainCanvas = document.getElementById("mainCanvas")
 
-    createShip(team, AI, isPlayer){
+    createShip(
+        team,
+        AIObject = {
+            AI: [],
+            coreName: "default"
+        },
+        activates = {},
+        effects = {},
+        isPlayer,
+        type = new Ship(true),
+        ascensions = [],
+        statsUpgrade = [],
+        defenseTypes = [],
+        effectsApply = []
+    ){
 
-        let newShip = ShipCreator.createShipFactory()
+        let newShip = ShipCreator.createShipFactory(type)
 
         newShip.team = team
 
         let haveAI = false
+
+        AIObject.AI = this.filterAI(newShip, AIObject.AI)
 
         if(team == "neutralTeam"){
 
             ObjectActivates.giveActivate(newShip, "weapon")
             ObjectActivates.giveActivate(newShip, "weapon")
 
-            newShip = AIC.giveAI(newShip, AI)
+            newShip = AIC.giveCoreAI(newShip, AIObject.AI, AIObject.coreName)
             haveAI = true
 
         }
 
-        if(AI && !isPlayer && team != "neutralTeam"){
-            newShip = AIC.giveAI(newShip, AI)
+        if(AIObject.AI && !isPlayer && team != "neutralTeam"){
+            newShip = AIC.giveCoreAI(newShip, AIObject.AI, AIObject.coreName)
             haveAI = true
-
-            ObjectActivates.giveActivate(newShip, "weapon")
-            ObjectActivates.giveActivate(newShip)
-            ObjectActivates.giveActivate(newShip)
-
         }
 
         if(isPlayer){
 
             newShip = KeyBoard.makeObjectInPlayerControl(newShip)
 
-            ObjectActivates.giveActivate(newShip, "weapon")
-            ObjectActivates.giveActivate(newShip, "defense")
-            ObjectActivates.giveActivate(newShip, "factory")
-            ObjectActivates.giveActivate(newShip, "special")
+            //newShip.energyRegen = 110
 
-            //new EffectsController().add(
-            //    "burn",
-            //    "effect",
-            //    {
-            //        "object": newShip,
-            //    },
-            //)
+            //newShip.life.math("-", 199)
+            //newShip.lifeRegen = -50/60
 
-            newShip = AIC.giveAI(newShip, AI)
+            newShip = AIC.giveCoreAI(newShip, AIObject.AI, AIObject.coreName)
+            //newShip = AIC.giveAI(newShip, AI)
             haveAI = false
+
+            //newShip.maxVel *= 1.5
 
         }
 
+        //bug?
+        this.mergeAscensionsInObject(
+            newShip,
+            ascensions
+        )
+
+        ObjectActivates.setActivates(newShip, activates)
+
         GameState.addObject(newShip, haveAI)
+
+        Effects.setEffects(newShip, effects)
+
+        for (let index = 0; index < statsUpgrade.length; index++) {
+
+            let statUpgrade = statsUpgrade[index]
+
+            MultiplyStats.multiply(
+                newShip,
+                statUpgrade[1],
+                {
+                    "normalStats": [statUpgrade[0]]
+                }
+            )
+
+        }
+
+        for (let index = 0; index < defenseTypes.length; index++) {
+
+            let defenseType = defenseTypes[index]
+
+            Damage.addDefense(
+                newShip,
+                defenseType[1],
+                defenseType[0],
+                defenseType[2],
+            )
+
+        }
+
+        Effects.addEffectsInActivates(newShip, effectsApply)
 
         return newShip
 
     }
 
-    createShipFactory(){
+    createShipFactory(ship){
 
-        let newShip = new Ship(true)
+        let newShip = ship
 
         newShip.ID = randomUniqueID()
         newShip.x = randomInteger(0, ShipCreator.mainCanvas.width)
         newShip.y = randomInteger(0, ShipCreator.mainCanvas.height)
 
         return newShip
+
+    }
+
+    filterAI(object, AI){
+        if(object.vel === undefined){
+
+            let index = AI.indexOf("movable")
+
+            if (index !== -1) {
+                AI.splice(index, 1)
+            }
+
+            index = AI.indexOf("escortAlly")
+
+            if (index !== -1) {
+                AI.splice(index, 1)
+            }
+
+            index = AI.indexOf("directionalDefense")
+
+            if (index !== -1) {
+                AI.splice(index, 1)
+            }
+            
+            index = AI.indexOf("flee")
+
+            if (index !== -1) {
+                AI.splice(index, 1)
+            }
+        
+        }
+        
+        if(object.resetVel === undefined){
+
+            let index = AI.indexOf("rotableTurret")
+
+            if (index !== -1) {
+                AI.splice(index, 1)
+            }
+
+            index = AI.indexOf("directionalDefense")
+
+            if (index !== -1) {
+                AI.splice(index, 1)
+            }
+
+        }
+        return AI
+    }
+
+    mergeAscensionsInObject(
+        object,
+        ascensions
+    ){
+
+        for (
+            let index = 0;
+            index < ascensions.length;
+            index++
+        ) {
+
+            let ascensionObject = AscensionScorer.getObject(
+                ascensions[index][0]
+            )
+
+            ascensionObject.selfBuild(
+                object
+            )
+
+            CloneObject.recursiveCloneAttribute(
+                ascensionObject,
+                object,
+            )
+
+            for (let key in ascensionObject) {
+
+                if(
+                    typeof(object[key]) == "number"
+                ){
+                    object[key] += ascensionObject[key]
+                }
+    
+            }
+
+        }
 
     }
 
