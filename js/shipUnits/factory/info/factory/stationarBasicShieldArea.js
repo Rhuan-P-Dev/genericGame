8a85e238d18041751540cc1b11d6bdb3a440468b
Rhuan P <147956448+Rhuan-P-Dev@users.noreply.github.com>
2024-10-30 11:43:18 -0300

    buff(stationary basic shield area - lifeTime): 20*60 -> 30*60

--- js/shipUnits/factory/info/factory/stationarBasicShieldArea.js
+++ js/shipUnits/factory/info/factory/stationarBasicShieldArea.js
@@ -34,7 +34,7 @@ export class StationarBasicShieldArea {
         "behavior": new FocusedTopDownBehavior().searchPriority,
         "statsMult": 0,
 
-        "lifeTime": 20*60,
+        "lifeTime": 30*60,
         
     }
 
