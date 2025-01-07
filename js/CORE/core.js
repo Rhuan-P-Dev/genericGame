f90b6ff668b66cf26c53977b4ea3e6d6b7e168c3
Rhuan P <147956448+Rhuan-P-Dev@users.noreply.github.com>
2025-01-07 18:04:11 -0300

    update core.js

--- js/CORE/core.js
+++ js/CORE/core.js
@@ -38,12 +38,24 @@ var HUD = ""
 var Statusbar = ""
 
 /* FRAME */
-import { FrameController } from "../frame/frameController.js"
+import { FrameController, setFrameOut } from "../frame/frameController.js"
 
 var Frame = ""
 
 /* GAMESTATE */
 import { GameStateController } from "../gameState/gameStateController.js"
+import { Prisp } from "../object/complex/special ship/prisp.js"
+import { Cryprwtrazghu } from "../object/complex/special ship/cryprwtrazghu/cryprwtrazghu.js"
+import { Cry } from "../object/complex/special ship/cryprwtrazghu/cry.js"
+import { Traz } from "../object/complex/special ship/cryprwtrazghu/traz.js"
+import { Ghu } from "../object/complex/special ship/cryprwtrazghu/ghu.js"
+import { Cryprw } from "../object/complex/special ship/cryprwtrazghu/cryprw.js"
+import { Fenix } from "../object/complex/special ship/fenix.js"
+import { TheBlessed } from "../object/complex/special ship/theBlessed.js"
+import { SelfSwarmMotherShip } from "../object/complex/special ship/selfSwarmMotherShip.js"
+import { BountyHunter } from "../object/complex/special ship/bountyHunter.js"
+import { SelfSwarmMelee } from "../shipUnits/factory/info/factory/selfSwarmMelee.js"
+import { Trazghu } from "../object/complex/special ship/cryprwtrazghu/trazghu.js"
 
 var GameState = ""
 
@@ -95,93 +107,210 @@ onInit(function(){
 
 function browseInit(){
 
-    ShipCreator.createShip("playerTeam", ["movable", "useActivates"], true).color = "green"
+    let eu = ShipCreator.createShip(
+        "playerTeam",
+        ["movable","escortAlly","useActivates"],
+        {
+            "weapon": [
+                //"black hole generator 1"
+                "mini world launcher"
+            ]
+        },
+        {
+            "add": {
+                "effect": [
+                    //"devour",
+                    //"attraction"
+                ]
+                //"onDamage": [
+                //    "the blessed effect: special"
+                //]
+            },
+            "apply": []
+        },
+        true,
+        new TheBlessed(true)
+    )
+    //eu.lifeTime = 1
+
+    for (let index = 0; index < 0; index++) {
+        ShipCreator.createShip("playerTeam", ["escortAlly","flee","useActivates","movable","areaSupport","directionalDefense","dodge"], false, new BountyHunter(true)).color = "blue"
+        //ShipCreator.createShip("playerTeam", ["movable","useActivates"], false).color = "blue"
+    }
+
+    //eu.x = 50
+    //eu.y = 50
+    //eu.currentYVel = 2
+
+    //let ele = ShipCreator.createShip("enemyTeam", ["movable", "useActivates"], false)
+    //ele.x = 815
+    //ele.y = 575
+
+    //let ele = ShipCreator.createShip("enemyTeam", ["accelerate"], false)
+    //ele.x = 815
+    //ele.y = 575
+
+    //ele = ShipCreator.createShip("enemyTeam", ["movable"], false)
+    //ele.x = 815
+    //ele.y = 575
+
+    //ele = ShipCreator.createShip("enemyTeam", ["missileV1"], false)
+    //ele.x = 815
+    //ele.y = 575
+
+    //ShipCreator.createShip("playerTeam", ["movable"], false, new BountyHunter(true))
+    //let ele = ShipCreator.createShip("playerTeam", [], false, new TheBlessed(true))
+
+    //new TheBlessed(true)
+    //eu.priority = -1
+    eu.color = "green"
+    eu.maxEnergy *= 10
+    eu.energyRegen *= 1011
+    //eu.lifeRegen = 601
+
+    //eu.lifeTime = 12
+
+    //ShipCreator.createShip("enemyTeam", [], false, new Fenix(true)).color = "red"
+
+    for (let index = 0; index < 1; index++) {
+
+        //let t = ShipCreator.createShip("enemyTeam", ["movable","useActivates","flee"], false)
+        //let t = ShipCreator.createShip("enemyTeam", ["movable","flee","dodge","useActivates"], false)
+        let t = ShipCreator.createShip("enemyTeam", [
+            //"movable","useActivates"
+        ],
+        {
+            //"weapon": ["random","random"],
+            //"factory": ["random"],
+        },
+        {
+            "add": {},
+            "apply": []
+        },
+        false,
+        new Trazghu(true)
+        )
+        t.color = "red"
+        //t.lifeRegen = 60
+        //t.maxLife = -1
+        //t.searchPriority.favoriteTargets = ["123"],
+        //t.searchPriority.favoriteTargetsObsession = {
+        //    "123": 0.5
+        //}
+
+        continue
+
+        let a = ShipCreator.createShip("enemyTeam", [], false, new TheBlessed(true))
+        let b = ShipCreator.createShip("enemyTeam", [], false, new Cryprwtrazghu(true))
+        let c = ShipCreator.createShip("enemyTeam", [], false, new Prisp(true))
+        let d = ShipCreator.createShip("enemyTeam", [], false, new Fenix(true))
+        let e = ShipCreator.createShip("enemyTeam", [], false, new BountyHunter(true))
+        let f = ShipCreator.createShip("enemyTeam", [], false, new SelfSwarmMotherShip(true))
+
+        a.x = 0
+        a.y = 0
+
+        b.x = 60
+        b.y = 0
+
+        c.x = 120
+        c.y = 0
+
+        d.x = 180
+        d.y = 0
+
+        e.x = 240
+        e.y = 0
+
+        f.x = 300
+        f.y = 0
+
+        //ShipCreator.createShip("playerTeam", [], false, new Cryprwtrazghu(true))
+
+        //ShipCreator.createShip("enemyTeam", ["movable","useActivates"], false)
+        //ShipCreator.createShip("enemyTeam", ["movable","useActivates"], false, new TheBlessed(true))
+        //ShipCreator.createShip("enemyTeam", ["movable","useActivates"], false, new Cryprwtrazghu(true))
+        //ShipCreator.createShip("enemyTeam", ["movable","useActivates"], false, new Prisp(true))
+        //ShipCreator.createShip("enemyTeam", ["movable","useActivates"], false, new Fenix(true))
+
+    }
+
+    for (let index = 0; index < 0; index++) {
+        //let enemy = ShipCreator.createShip("enemyTeam", ["movable", "useActivates"], false, new SelfSwarmMotherShip(true))
+        //enemy.color = "red"
+        let aly = ShipCreator.createShip("playerTeam", ["movable", "useActivates"], false, new TheBlessed(true))
+        setFrameOut(() => {
+            ScreenRender.setFocusObject(aly)
+        },1)
+        aly.color = "blue"
+    }
+
+
+    //ShipCreator.createShip("enemyTeam", ["movable", "useActivates"]).color = "red"
+    //ShipCreator.createShip("enemyTeam", ["movable", "useActivates"]).color = "red"
+    //ShipCreator.createShip("enemyTeam", ["movable", "useActivates"]).color = "red"
 
     //ShipCreator.createShip("enemyTeam", ["movable","useActivates"]).color = "red"
     //ShipCreator.createShip("enemyTeam", ["accelerate"]).color = "purple"
-    ShipCreator.createShip("enemyTeam", ["dummy"]).color = "purple"
+    //ShipCreator.createShip("enemyTeam", ["movable","useActivates"]).color = "purple"
 
     setInterval(() => {
 
-        ShipCreator.createShip("playerTeam", ["movable", "useActivates"]).color = "blue"
-        ShipCreator.createShip("enemyTeam", ["movable", "useActivates"]).color = "red"
+        return
 
-    }, 5000)
+        ShipCreator.createShip("playerTeam", ["escortAlly","flee","useActivates","movable","areaSupport","directionalDefense","dodge"]).color = "blue"
+        ShipCreator.createShip("enemyTeam", ["escortAlly","flee","useActivates","movable","areaSupport","directionalDefense","dodge"]).color = "red"
+
+    }, 10000)
 
     setInterval(() => {
+
+        return
 
         // arena closer
         console.log("close...")
 
         for (let index = 0; index < 100; index++) {
 
-            ShipCreator.createShip("neutralTeam", ["missileV1","useActivates"]).lifeTime = 10*60
+            ShipCreator.createShip("neutralTeam", ["escortAlly","flee","useActivates","movable","areaSupport","directionalDefense","dodge"]).lifeTime = 10*60
 
         }
 
-    }, 50000)
+    }, 60000)
 
     KeyBoard.addTriggers()
 
 }
 
+// MULTYPLAYER?
 
-// EMPACOTAR!
-// ONDAS DE INIMIGOS INFINITAS, CADA VEZ MAIS FORTE!
+// O 'BOSS FINAL!'
 
-// O SYSTEMAS DE ONDAS SIRA FIXO? OU IGUAL O RIMWORLD?
-// MINIMO: arma, defeça, special, factory
-// COMFORME AS ONDAS VÃO AS NAVES INIMIGAR FICAN MAIS INTELIGENCES!
-// ALEATORIMENTE AS NAVES PEGAR AUSILIADOERWES: dark energy, swarm
-// efeitos!
+// O TEST QUANTUM BOMB KILL ALIIYES???
 
-// VC GANHA DINHEIRO - E % VAI SER CONVERTUIDO PARA DINHEIRO PARA BUFF PERM!
-// DINHEIRO você combra: buffs, naves, ativadores, INTELIGENCIA, efeitos
-// BUY! AS AUSILIADOERWES: dark energy, swarm
+// defineTypeOfLoarderName................................................. Loarder
 
+// OTIMIZAR A FUMAÇA
 
 // make AI more letal, sniper, know?
 // make the AI desviar
 
-// LLM ?!!
-// BIG SUPPORT BASE?
-
-// dar uma leve polida
-
-
-
-
-
-
-
-
-// seifador ABSOLÇUTO!
-
 // nave s: nulificador
-
+// raio NULIFICADOR
+// retira stats maximos e regens
+// bola nulificadore que crese!
 // um tipo de nulificação abusoluta
 
 // fazer um "movable" que temta desviar do objeto
 
-// overclock - kill
-
 // talvez refazer todo para emcaixar um sistema de zoom decente? que multiplica o X e Y
-
-// os auto wepoans não estam sendo apgados quanso se morre para o special
 
 //se a espanão de domini o teleportase parta outra parte do mapa?
 
 /*
-
-os tiros da fenix serem embuidos de fogo!
-
 O DEUS DA ESCURIDÃO ABISAL! uma nave vai perdendo o HP a escuridão vai tomando! taus etapsas
 NAVE TRISTE!!!
-
 */
-
-
-// atualiuzar os efeitos que redireconam dano
 
 // UMA NAVE ESPECIAL DEVERIAR CONSEGUIR CLONAR SE?
 
@@ -197,8 +326,6 @@ re-estruturar os objetos import e etc []
 
 quando for adicioanr um objeto no jogo, loopers como o 'ruler' dever pergar o objeto e analizar para colocar no local correto
 
-qualquer [.life -= X] esta errado!
-
 o 'feito' do buraco necro é solo/não da para apagar! de crecer/diminuir
 
 // um tipo de factory, que criar algo com pontos obetidos por tempo / kills []
@@ -213,18 +340,7 @@ uma arma em formato de cone que congela/slowdon o alvo
 
 criar uma nova categoria chamada suporte?
 
-
-
-
-
-
-
-
-
-
-
-
-
+// rachadura - dar dano? tira maximos? e deixa vuneravel
 
 um missil anti-naves rapidas
 
@@ -232,6 +348,13 @@ item que chama outras naves como apoio
 
 uma arma que faz um overclock forçado
 uma arma que control suya nave enemiga
+
+empreratriz louca - covnert enemys into madness allyes 
+ela tem eenrgia cursed
+the main attack is a lance - the lançe does agony damage - the lance is durable(has HP)
+she can lance a homing lance.
+she can lance a homing curse to convert enemys and make the mad - the curse don't have a grapic it has a animation of a curse
+she has a special, it can deal the +50% damage has lance, multiples times
 
 
 */
@@ -253,8 +376,6 @@ optimizar o sistema de colisoes! []
 
 /* > ADD <
 
-ALGUEN DEVE GUARDAR QUAILS STATS DEVEM SER MULT E OUTRAS COISAS / DEVE TER SCHEDULER ETC / CERTOS STATUS NÃO DEVEM SOFRER TANTO! []
-
 add this > // wrong!
 object.x = object.owner.x + object.xOffset
 object.y = object.owner.y + object.yOffset
@@ -264,67 +385,38 @@ object.y = object.owner.y + object.yOffset
 /* > RE <
 returnArrayWithAlllObjectsOfTeams( // create a cache, think! same params of request use cache! []
 
-o jeito que o speial do a bensoado funcinar esta earrado
-
 uma nave com dodge não consegue prever pouco dano por frame! caso uma mae "boba" ataque não vai se devender coretametne.
 
 */
 
 /* > BUG <
 
-as illusõesa NAõ iluden!
-
 // exesso de uso de memoria ram
 object = null?
-
-bug: casi vc mude de time suas armar automatica não mudão!
-
-o "boom" esta em vc qunado clona!
 
 modulo que faz os clones NÃO esta clonado os modificadores apenas as armas
 
 */
 
-// nave special: um nave que com opaser dos segundos fica mais forter! [/]
-// naves especialis tipo: um namo com energia infinita! []
-
-//um tipo de objeto especial que NÃO morre []
-
 // um nave FODA porem... tem um caçador de recompesans []
-
-// uma nave special: player no spewna gera algo, toda vez que o player morre ele renaçe no algo []
 
 // uma nave special: 'só existe uma ração' qunado morre tem % de chante de renacer, começa em: 0 e almenta a cada segundo []
 // um efeito com % de ativar []
 
-// "depois que eu morrer outros virão..." []
-// um tipo especial de "nave?" que tem varias facetas em qunato um estiver viva todas vivem? / renasão []
-
 // uma nave tipo 'gilgamesh', se a prioridade da nave enemiga for maior... se o HP estiver baixo... []
-// uma nave tipo "gojo joven" []
-
-// um nave que "sempre" volta?????? [?] e depois que morre um ultima vez, essa nave nao pode ir para fora? / uma versão alternativa
 
 // parar o tempo? []
 
-// uma nave que se adaptar ao dano levado []
-// GENERAL DIVINO! []
+// your nemesys []
+
+// uma nave que se adaptar ao dano levado [x] ? []
 
 // uma nave que tira a multiplicador nas coisas? []
 
-// uma nave especial qndo morre cria um clone de si mesmo com menos STATS. []
-// quando algo morre divide em dois? []
-
 // nave: capaz de realizar milagres periodicamente [] duplica tudo!(aliados)
-// nave: "final clock" []
 
-// nave: "rei dos fracos"
 // "peoes do rei" periodicamente converta algo []
 // nave: o deus das factorys
-
-// nave?: "não sinto o peso dos meus atos"
-
-// arena closer...
 
 //uma nave uqe fala "wake up", se ele morre ela respauwna, caso morra denovo antes do "wake up" ela morre
 
@@ -334,16 +426,74 @@ modulo que faz os clones NÃO esta clonado os modificadores apenas as armas
 
 //um activate de cura que usa MUINTA energia para quando a vids cair por um serto valor recupera vida
 
-//uma nave que tem um "poll" de poder se esse poll checkar o maximo ela transende, essse poll consume para o ataque e para a defesa
+// um necro mancer que revive naves/zombies a cada !!!onKill!!!
 
-// chara like kill XP!
+            /*
 
-// uma nave que é um "destribuidor"
+                the X_ACTIVATE call drones to help every 2m
+                the X_ACTIVATE was the 100% to call drones
+                the X_ACTIVATE was the 75% to call drones v2
+                the X_ACTIVATE was the 50% to call big drones
+                the X_ACTIVATE was the 25% to call a ship
+                the X_ACTIVATE was the 10% to call police
 
-// sans? gast blast 'drone'
+                config = {
+                    "objectClass": Drone,
+                    "AI": ["movable","useActivates"],
+                    "activates": {
+                        "weapon": ["piston 1"],
+                    },
+                    "behavior": new FocusedNearBehavior().searchPriority,
+                    "statsMult": 0,
+                }
+
+                  setFrameOut(
+                () => {
+
+                    if(
+                        !GameState.getObject(updateThis.ID)
+                    ){return}
+
+                    /*
+
+                    "configs": [
+                        new MovableSaferPerimeter1().config,
+                        new MovableSaferPerimeter1().config,
+                        new SaferPerimeter1().config,
+                    ],
+
+                    "repeat": 1,
+                    "dispersion": 1,
+                    "velMult": 0,
+
+
+                    GenericEffects.effectsList["create objects"]({
+                        "configs": [
+                            {
+                                "objectClass": Drone,
+                                "AI": ["movable","useActivates"],
+                                "activates": {
+                                    "weapon": ["piston 1"],
+                                },
+                                "behavior": new FocusedNearBehavior().searchPriority,
+                                "statsMult": 0,
+                            }
+                        ],
+                        "repeat": 1,
+                        "dispersion": 1,
+                        "velMult": 0,
+                    })
+
+                },
+                2*10*60,
+                -1
+            )
+
+
+            */
+
 
 var previousTime = performance.now()
-
 
 function calculateFPS() {
 
